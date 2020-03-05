435924dae862cc4126f378212643e004106ab270
Abhishek5101 <miabhishekkulkarni@gmail.com>
2020-03-04 22:58:56 -0800

    Added sets Time Complexity

--- Code/sets.py
+++ Code/sets.py
@@ -10,14 +10,23 @@ class Set:
 				self.add(element)
 		
 	def size(self):
+		"""
+		Time Complexity --> O(1)
+		"""
 		return self.size
 	
 	def contains(self, element):
 		"""Return a boolean if the element is in the Set"""
+		"""
+		Time Complexity --> O(1)
+		"""
 		return self.hash.contains(element)
 		
 	def add(self, element):
 		"""Add an element to the set"""
+		"""
+		Time Complexity --> O(1) Except when resize is called. Then O(n)
+		"""
 		self.size += 1
 		if not self.hash.contains(element):
 			return self.hash.set(element, element)
@@ -25,6 +34,9 @@ class Set:
 
 	def intersection(self, other_set):
 		"""Return the set of elements that are common in both sets"""
+		"""
+		Time Complexity --> O(n)
+		"""
 		new_set = Set()
 		for each_element in self.hash.values():
 			if other_set.contains(each_element):
@@ -32,6 +44,9 @@ class Set:
 		return new_set
 	
 	def union(self, other_set):
+		"""
+		Time Complexity --> O(n+m) since we traverse both sets
+		"""
 		new_set = Set()
 		for element in self.hash.values():
 			new_set.add(element)
@@ -41,6 +56,9 @@ class Set:
 		return new_set
 	
 	def remove(self, element):
+		"""
+		Time Complexity --> O(1). & O(n) if we need to resize
+		"""
 		if not self.hash.contains(element):
 			raise KeyError("Can not delete element that doesn't exist")
 		self.hash.delete(element)
@@ -48,6 +66,9 @@ class Set:
 	
 	def difference(self, other_set):
 		"""Return a new set that is the difference of this set and other_set."""
+		"""
+		Time Complexity --> O(n+m)  Traversing the self set & the other set to find common
+		"""
 		new_set = Set()
 		for element in self.hash.values():
 			new_set.add(element)
@@ -60,6 +81,10 @@ class Set:
 	
 	def is_subset(self, other_set):
 		"""Return a boolean indicating whether other_set is a subset of this set."""
+		"""
+		Time Complexity --> O(n) traverse the whole set to see if it contains all the elements
+		"""
+
 		return bool([element for element in other_set.hash.values() if self.contains(element)])
 
 
